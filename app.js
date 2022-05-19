295b35fdc87fcb71fce691a966006f38acf3e3f9
Anahi Quintero <anitaquintero99@gmail.com>
2022-05-19 16:56:36 -0500

    Add new apy-key

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-const API_URL_RANDOM = new URL("https://api.thecatapi.com/v1/images/search");
+const API_URL_RANDOM = new URL(
+  "https://api.thecatapi.com/v1/images/search?limit=3&api_key=f098c277-9d2a-44ef-b00e-9135d244716e"
+);
+
+const imgCat1 = document.getElementById("img1");
+const imgCat2 = document.getElementById("img2");
+const imgCat3 = document.getElementById("img3");
 
 async function loadRandomCats() {
   try {
     const response = await fetch(API_URL_RANDOM);
-    const data = await response.data();
+    const data = await response.json();
 
-    console.log(data);
-  } catch (error) {}
+    imgCat1.src = data[0].url;
+    imgCat2.src = data[1].url;
+    imgCat3.src = data[2].url;
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+loadRandomCats();
